e5a2253437c4dfbac6843ff9f413ef1d7d77fa42
Javi R <4920956+rameerez@users.noreply.github.com>
2024-09-25 10:17:15 +0800

    Initial commit

---
+++ lib/dashboards.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dashboards/version"
+
+module Dashboards
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dashboards/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Dashboards
+  VERSION = "0.1.0"
+end
